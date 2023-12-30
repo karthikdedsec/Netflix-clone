746266ec8088e9b62380edce1ccd8049880f64cb
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2023-12-30 21:17:51 +0530

    finish the movie and row component and set up the scroll slide

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,53 @@
-function Row({ title, fetchURL }) {
+import axios from "axios";
+
+import { useEffect, useState } from "react";
+import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
+function Row({ title, fetchURL, rowId }) {
+  const [movies, setMovies] = useState({});
+
+  useEffect(() => {
+    const getMovies = async () => {
+      await axios.get(fetchURL).then((response) => {
+        setMovies(response.data.results);
+      });
+    };
+    getMovies();
+  }, [fetchURL]);
+
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+        <div
+          id={"slider" + rowId}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
+          {movies && movies.length > 0
+            ? movies.map((item, id) => <Movie key={id} item={item} />)
+            : ""}
+        </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+      </div>
     </>
   );
 }
